206d0bf5bb0ea0d2640b5848e5269edb6e91b5a5
deltartificial <deltartificial@protonmail.com>
2025-02-10 23:18:48 +0100

    feat(mod): add modules

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,1 +1,12 @@
 // @dfns-sdk-rs/src/api/mod.rs
+
+mod auth;
+mod exchanges;
+mod networks;
+mod permissions;
+mod policies;
+mod signers;
+mod staking;
+mod wallets;
+mod webhooks;
+
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,1 +1,11 @@
 // @dfns-sdk-rs/src/models/mod.rs
+
+mod auth;
+mod exchanges;
+mod networks;
+mod permissions;
+mod policies;
+mod signers;
+mod wallets;
+mod webhooks;
+
