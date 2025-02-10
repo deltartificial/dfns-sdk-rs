5ccf12cc27b6a36e315cfcf0fdc2dc766aa08796
deltartificial <deltartificial@protonmail.com>
2025-02-10 23:21:29 +0100

    feat(utils): add base64 utils

--- src/utils/base64.rs
+++ src/utils/base64.rs
@@ -1,1 +1,23 @@
 // @dfns-sdk-rs/src/utils/base64.rs
+
+use base64::{Engine, engine::general_purpose};
+
+pub fn to_base64(input: impl AsRef<[u8]>) -> String {
+    general_purpose::STANDARD.encode(input)
+}
+
+pub fn from_base64(encoded: &str) -> Result<Vec<u8>, base64::DecodeError> {
+    general_purpose::STANDARD.decode(encoded)
+}
+
+pub fn to_base64_url(input: impl AsRef<[u8]>) -> String {
+    general_purpose::URL_SAFE_NO_PAD.encode(input)
+}
+
+pub fn from_base64_url(encoded: &str) -> Result<Vec<u8>, base64::DecodeError> {
+    let padded = match encoded.len() % 4 {
+        0 => encoded.to_string(),
+        n => format!("{}{}", encoded, "=".repeat(4 - n))
+    };
+    general_purpose::URL_SAFE.decode(&padded)
+}
