25a984582ced84723df3f2eb81867e7cdebc2d6e
deltartificial <deltartificial@protonmail.com>
2025-02-21 10:49:52 +0100

    refactor(tests): reorder module declarations in test mod.rs files

--- tests/unit/api/auth/mod.rs
+++ tests/unit/api/auth/mod.rs
@@ -1,4 +1,4 @@
-mod client_test;
 /// @dfns-sdk-rs/tests/unit/api/auth/mod.rs
+mod client_test;
 mod delegated_client_test;
 mod types_test;
--- tests/unit/api/webhooks/mod.rs
+++ tests/unit/api/webhooks/mod.rs
@@ -1,5 +1,5 @@
-mod client_test;
 /// @dfns-sdk-rs/tests/unit/api/webhooks/mod.rs
+mod client_test;
 mod delegated_client_test;
 mod types_fn_test;
 mod types_test;
