6b831a4fb2743e1695cf09b5637442fc6a0c7430
deltartificial <deltartificial@protonmail.com>
2025-02-21 10:04:05 +0100

    test(error): add comprehensive error trait and conversion tests

--- tests/unit/error_test.rs
+++ tests/unit/error_test.rs
@@ -2,6 +2,8 @@
 mod tests {
     use dfns_sdk_rs::error::{DfnsError, PolicyPendingError};
     use serde_json::json;
+    use std::error::Error;
+    use std::fmt::Display;
     use url::Url;
 
     #[test]
@@ -45,11 +47,34 @@ mod tests {
     }
 
     #[test]
-    fn test_invalid_header_conversion() {
+    fn test_error_traits() {
+        let error = DfnsError::new(400, "test", None);
+        let _: &dyn Error = &error;
+        let _: &dyn Display = &error;
+
+        let policy_error = PolicyPendingError::new(None);
+        let _: &dyn Error = &policy_error;
+        let _: &dyn Display = &policy_error;
+    }
+
+    #[test]
+    fn test_error_conversions() {
+        let policy_error = PolicyPendingError::new(None);
+        let _: DfnsError = policy_error.into();
+
+        let client = reqwest::Client::new();
+        let invalid_url = "ht tp://invalid.com";
+        let req_error = client.get(invalid_url).build().unwrap_err();
+        let _: DfnsError = req_error.into();
+
+        let parse_error = Url::parse("invalid-url").unwrap_err();
+        let _: DfnsError = parse_error.into();
+
         let header_error = reqwest::header::HeaderValue::from_str("invalid\nheader").unwrap_err();
-        let dfns_error: DfnsError = header_error.into();
-        assert_eq!(dfns_error.http_status, 400);
-        assert!(dfns_error.message.contains("Invalid header value"));
+        let _: DfnsError = header_error.into();
+
+        let json_error = serde_json::from_str::<serde_json::Value>("invalid").unwrap_err();
+        let _: DfnsError = json_error.into();
     }
 
     #[test]
